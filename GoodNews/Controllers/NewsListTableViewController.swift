f889be9c9eed8ac697e649acf0d5b1bb50031b36
dommontalto <dominic.montalto@fusionautomation.com>
2019-07-17 11:02:42 +0100

    First commit

---
+++ GoodNews/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,62 @@
+//
+//  NewsListTableViewController.swift
+//  GoodNews
+//
+//  Created by Mohammad Azam on 2/5/19.
+//  Copyright © 2019 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+    
+    private var articleListVM: ArticleListViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let url = URL(string: "https://newsapi.org/v2/everything?q=apple&from=2019-07-16&to=2019-07-16&sortBy=popularity&apiKey=0a25ef30763d41e19213d35cba25b3de")!
+        
+        Webservice().getArticles(url: url) { articles in
+            
+            if let articles = articles {
+                
+                self.articleListVM = ArticleListViewModel(articles: articles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell not found")
+        }
+        
+        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+        
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        return cell 
+    }
+    
+}
